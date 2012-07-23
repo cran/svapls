c251540098cf678bb2bc72774790369ccacf0733
Sutirtha Chakraborty <s0chak10@louisville.edu>
2012-07-23 00:00:00 +0000

    version 1.0

---
+++ R/hfp.R
@@ -0,0 +1,10 @@
+hfp <-
+function (obj,gen,ind,Y){   
+    genes <- Y[,1]
+    num <- length(gen)
+    gen.index <- rep(0,num)
+    for (i in 1:num) gen.index[i] <- which(genes == gen[i]) 
+    W_imp <- obj$PLS.imp
+    if (max(W_imp) != 0) heatmap(t(W_imp[gen.index,ind]),Rowv = NA,Colv = NA,labRow=as.character(ind),labCol=gen,xlab="Genes",ylab="Specified subjects")
+    if (max(W_imp) == 0) print("No trace of hidden subject-specific variation is found in the data")
+}
